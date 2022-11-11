038411af4a7bdd97e5f9b3045693168b415e6b90
gabrielprns <64814557+gabrielprns@users.noreply.github.com>
2022-11-11 20:23:37 -0300

    Ajustes

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,20 +1,21 @@
 <header class="header-area">
+    <link rel="stylesheet" href="{{ asset('css/header.css') }}">
         <!-- Top Header Area -->
         <div class="top-header-area">
-            <div class="container h-100">
+            <div class="h-100 mr-3 ml-3 container-max-widths">
                 <div class="row h-100 align-items-center">
                     <div class="col-12 d-flex justify-content-between">
                         <!-- Logo Area -->
                         <div class="logo">
-                            <a href="/"><img src="img/logo-mge-top.png" alt=""></a>
+                            <a href="/"><img class="logo-size" src="img/logo-mge-top.png" alt=""></a>
                         </div>
 
                         <!-- Top Contact Info -->
-                        <div class="top-contact-info d-flex align-items-center">
-                            <a href="#" data-toggle="tooltip" data-placement="bottom" title="xxxl"><img src="img/core-img/location.png" alt="">
-                                <span style="font-size: 60%;">Rua Olga Moretti Ferrari, 228 - Vila Santa Lucia</span>
+                        <div class="top-contact-info d-flex align-items-center w-200 mr-4 justify-content-end">
+                            <a href="#" data-toggle="tooltip" data-placement="bottom" title="xxxl"><img class="location-size"  src="img/core-img/location.png" alt="">
+                                <span class= "texts">Rua Olga Moretti Ferrari, 228 - Vila Santa Lucia</span>
                             </a>
-                            <a href="#" data-toggle="tooltip" data-placement="bottom" title="mgetelecom@gmail.com"><img src="img/core-img/message.png" alt=""> <span style="font-size: 60%;">mgenetwork@gmail.com</span></a>
+                            <a href="#" data-toggle="tooltip" data-placement="bottom" title="mgetelecom@gmail.com"><img class="email-size" src="img/core-img/message.png" alt=""> <span class= "texts">mgenetwork@gmail.com</span></a>
                         </div>
                     </div>
                 </div>
@@ -22,11 +23,11 @@
         </div>
 
         <!-- Navbar Area -->
-        <div class="credit-main-menu" id="sticker">
-            <div class="classy-nav-container breakpoint-off">
-                <div class="container">
+        <div class="col-lg-12" id="sticker">
+            <div class="classy-nav-container breakpoint-off ">
+                <div class="container-sm">
                     <!-- Menu -->
-                    <nav class="classy-navbar justify-content-between" id="creditNav">
+                    <nav class="classy-navbar justify-content-between " id="creditNav">
 
                         <!-- Navbar Toggler -->
                         <div class="classy-navbar-toggler">
@@ -42,22 +43,25 @@
                             </div>
 
                             <!-- Nav Start -->
-                            <div class="classynav">
+                            <div class="classynav ">
                                 <ul class="mt-4">
-                                    <li><a href="welcome" style="font-size: 1.3em">Home</a></li>
-                                    <li><a href="about" style="font-size: 1.3em">Empresa</a></li>
-                                    <li><a href="#" style="font-size: 1.3em">Planos</a></li>
-                                    <li><a href="coverage" style="font-size: 1.3em">Área de Cobertura</a></li>
-                                    <li><a href="#" style="background-color: green; font-size: 1.3em; border-radius: 10em;">Central do assinante</a></li>
+                                    <li ><a href="welcome" style=" color:#6DBE4B; font-size:20px; margin-right: 20px;">Home</a></li>
+                                    <li ><a href="about" style="color:#6DBE4B; font-size:20px; margin-right: 20px; ">Empresa</a></li>
+                                    <li ><a href="#" style="color:#6DBE4B; font-size:20px; margin-right: 20px;">Planos</a></li>
+                                    <li ><a href="coverage" style="color:#6DBE4B; font-size:20px; margin-right: 20px;">Área de Cobertura</a></li>
+                                    <li ><a href="#" style="color:white; background-color: #6DBE4B; font-size:20px; border-radius: 10em;">Central do assinante</a></li>
                                 </ul>
                             </div>
                             <!-- Nav End -->
                         </div>
 
                         <!-- Contact -->
-                        <div class="contact">
-                            <a href="#" style="font-size:1.2em"><img src="img/core-img/call2.png" alt="">  (11) 97967-8053 / 5892-3779   </a>
-                        </div>
+                        <nav class="navbar mt-per d-none d-lg-block " >
+                            <div class="contact ">
+                                <a href="#" style="font-size:15px"><img src="img/core-img/call2.png" alt="">  (11) 97967-8053 / 5892-3779   </a>
+                            </div>
+                        </nav>
+                        
                     </nav>
                 </div>
             </div>
